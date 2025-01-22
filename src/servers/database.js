ea65ffe9d12e5f30207e795e23ae319097278494
Darcy Campbell <darcycampbellatwork@outlook.com>
2025-01-22 12:07:53 +0800

    Add env variables

--- src/servers/database.js
+++ src/servers/database.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
